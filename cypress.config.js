f50c71b46efc5346d071bdaa04c5bb67cc349458
Pitoparra17 <juan.parragreglog@gmail.com>
2025-09-02 11:24:31 -0500

    inhabilitar video

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   retries: 1,//realiza en el intento 1 y el intento 2
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
             allureWriter(on, config);
